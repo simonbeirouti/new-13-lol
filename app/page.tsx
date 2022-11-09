9511c4564216be2242e9bce652b70cda6e718f93
simonbeirouti <hello@simonbeirouti.com>
2022-11-09 21:10:22 +1100

    show example of suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import { Suspense } from "react";
+import ToDosList from "./(user)/todos/ToDosList";
+
 export default function Home() {
   return (
-    <div className="p-5">
-        I am the captain now
-    </div>
+    <>
+      <Suspense fallback={<p>Loading the todos...</p>}>
+        <h1>Loading Todos</h1>
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+            <ToDosList />
+        </div>
+      </Suspense>
+      
+      <Suspense fallback={<p>Loading the shopping trolley...</p>}>
+        <h1>Loading shopping trolley</h1>
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+            <ToDosList />
+        </div>
+      </Suspense>
+    </>
   )
 }
