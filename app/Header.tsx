ad7c605dab2d8d576975606644e4bfff8d0aced6
simonbeirouti <hello@simonbeirouti.com>
2022-11-09 21:09:59 +1100

    add search to header

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,11 +4,14 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="p-5 bg-blue-500">
-        <Link className="px-2 bg-white text-blue-500 rounded-md" href="/">
+        <Link className="p-2 bg-white text-blue-500 rounded-md" href="/">
             Home
         </Link>
-        <Link className="px-2 bg-white text-blue-500 rounded-md" href="/todos">
+        <Link className="p-2 bg-white text-blue-500 rounded-md" href="/todos">
             ToDos
+        </Link>
+        <Link className="p-2 bg-white text-blue-500 rounded-md" href="/search">
+            Search
         </Link>
     </header>
   )
