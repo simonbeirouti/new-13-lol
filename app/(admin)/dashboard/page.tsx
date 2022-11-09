49e4eea17441eb3b583cf5946ddc47b458057130
simonbeirouti <hello@simonbeirouti.com>
2022-11-09 21:10:48 +1100

    create page to show pages working

---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="p-5">
+        I am the captain now
+    </div>
+  )
+}
---
+++ app/(admin)/developer/page.tsx
@@ -0,0 +1,7 @@
+export default function Developer() {
+  return (
+    <div className="p-5">
+        I am the captain now
+    </div>
+  )
+}
